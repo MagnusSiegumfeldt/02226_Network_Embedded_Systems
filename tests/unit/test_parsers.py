8906914d2d16c8ed764067a4ec3a519c6903c347
Magnus Siegumfeldt <m.ks@outlook.dk>
2024-11-27 12:53:43 +0100

    test work

--- tests/unit/test_parsers.py
+++ tests/unit/test_parsers.py
@@ -3,6 +3,13 @@ from Parser.StreamParser import StreamParser
 from Topology.EndSystem import EndSystem
 from Topology.Switch import Switch
 
+
+def check_device(topology, n, t):
+    return any(type(topology.devices[k]) is t and n == topology.devices[k].name for k in topology.devices.keys())
+
+def check_link(topology, e1, e2):
+    return any((e1 == a and e2 == b) or (e1 == b and e2 == a) for (a, b) in topology.links.keys())
+
 # Verifies that topology is parsed correctly
 def test_parse_topology_1():
     parser = TopologyParser()
@@ -10,38 +17,63 @@ def test_parse_topology_1():
     topology = parser.parse("./tests/inputs/simple_topology.csv")    
     assert(len(topology.devices) == 4)
     assert(len(topology.links) == 3)
-
-    check_device = lambda n, t: any(type(topology.devices[k]) is t and n == topology.devices[k].name for k in topology.devices.keys())
-    check_link =   lambda e1, e2: any((e1 == a and e2 == b) or (e1 == b and e2 == a) for (a, b) in topology.links.keys())
     
     # Check Topology
-    assert(check_device("A", EndSystem))
-    assert(check_device("B", Switch))
-    assert(check_device("C", EndSystem))
-    assert(check_device("D", EndSystem))
+    assert(check_device(topology, "A", EndSystem))
+    assert(check_device(topology, "B", Switch))
+    assert(check_device(topology, "C", EndSystem))
+    assert(check_device(topology, "D", EndSystem))
 
-    assert(check_link("A", "B"))
-    assert(check_link("B", "C"))
-    assert(check_link("B", "D"))
+    assert(check_link(topology, "A", "B"))
+    assert(check_link(topology, "B", "C"))
+    assert(check_link(topology, "B", "D"))
 
-    # Wrong checks
-    assert(not check_device("A", Switch))
-    assert(not check_device("B", EndSystem))
-    assert(not check_link("A", "C"))
-    assert(not check_link("A", "D"))
-    assert(not check_link("C", "D"))
+    # Check nothing non-defined exists
+    assert(not check_device(topology, "A", Switch))
+    assert(not check_device(topology, "B", EndSystem))
+    assert(not check_link(topology, "A", "C"))
+    assert(not check_link(topology, "A", "D"))
+    assert(not check_link(topology, "C", "D"))
 
 
-def test_parse_topology_2():
-    pass
-    """
+def test_routing_topology_2():
     parser = TopologyParser()
 
-    topology = parser.parse("./tests/inputs/simple_topology.csv")    
-    print(topology.devices)
-    assert(len(topology.devices) == 328)
-    assert(len(topology.links) == 409)
-    """ 
+    topology = parser.parse("./tests/inputs/medium_topology.csv")    
+    assert(len(topology.devices) == 8)
+    assert(len(topology.links) == 8)
+    
+    # Check Topology
+    assert(check_device(topology, "A", EndSystem))
+    assert(check_device(topology, "B", EndSystem))
+    assert(check_device(topology, "C", EndSystem))
+    assert(check_device(topology, "D", Switch))
+    assert(check_device(topology, "E", Switch))
+    assert(check_device(topology, "F", Switch))
+    assert(check_device(topology, "G", EndSystem))
+    assert(check_device(topology, "H", Switch))
+
+    assert(check_link(topology, "A", "D"))
+    assert(check_link(topology, "B", "D"))
+    assert(check_link(topology, "C", "D"))
+    assert(check_link(topology, "C", "F"))
+    assert(check_link(topology, "D", "E"))
+    assert(check_link(topology, "E", "G"))
+    assert(check_link(topology, "F", "H"))
+    assert(check_link(topology, "G", "H"))
+
+    # Check nothing non-defined exists
+    assert(not check_device(topology, "A", Switch))
+    assert(not check_device(topology, "B", Switch))
+    assert(not check_device(topology, "C", Switch))
+    assert(not check_device(topology, "D", EndSystem))
+
+    assert(not check_device(topology, "null", Switch))
+    assert(not check_device(topology, "null", EndSystem))
+
+    assert(not check_link(topology, "A", "C"))
+    assert(not check_link(topology, "A", "E"))
+    assert(not check_link(topology, "G", "F"))
 
 
 # Verifies that streams are parsed correctly.
