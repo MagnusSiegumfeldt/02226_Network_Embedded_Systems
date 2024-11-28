e532dedcb54762794f3659a32b86a112282968a8
Magnus Siegumfeldt <m.ks@outlook.dk>
2024-11-28 15:33:30 +0100

    delay test corresponding with report

--- src/Parser/TopologyParser.py
+++ src/Parser/TopologyParser.py
@@ -11,14 +11,14 @@ class TopologyParser:
 
                 type = entries[0]
                 if type == "SW":
-                    name, ports, domain = entries[1], int(entries[2]), int(entries[3]) if len(entries) == 4 else None
+                    name, ports, domain = entries[1], int(entries[2]), int(entries[3]) if len(entries) == 4 and entries[3] != '' else None
                     topology.add_switch(name, ports, domain)
                     
                 elif type == "ES":
-                    name, ports, domain = entries[1], int(entries[2]), int(entries[3]) if len(entries) == 4 else None
+                    name, ports, domain = entries[1], int(entries[2]), int(entries[3]) if len(entries) == 4 and entries[3] != '' else None
                     topology.add_end_system(name, ports, domain)
 
                 elif type == "LINK":
-                    name, ep1, port1, ep2, port2, domain = entries[1], entries[2], int(entries[3]), entries[4], int(entries[5]), int(entries[6]) if len(entries) == 7 else None
+                    name, ep1, port1, ep2, port2, domain = entries[1], entries[2], int(entries[3]), entries[4], int(entries[5]), int(entries[6]) if len(entries) == 7 and entries[6] != '' else None
                     topology.add_link(name, ep1, port1, ep2, port2, domain)
             return topology
